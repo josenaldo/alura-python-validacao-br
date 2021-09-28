2742061c569e688130ea2081265994e343bb8afc
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-28 09:37:47 -0300

    Curso "Python Brasil: Validação de dados no padrão nacional" Aula 04 Formatação de datas

---
+++ br_datas.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import locale
+
+
+class DataBr:
+
+    def __init__(self):
+        self.__criado_em = datetime.today()
+        locale.setlocale(locale.LC_TIME, "pt_BR")
+
+    def __repr__(self):
+        return f"DataBr(criado_em='{self.criado_em.strftime('%d/%m/%Y %H:%M:%S')}')"
+
+    def __str__(self):
+        return self.data_formatada()
+
+    @property
+    def criado_em(self):
+        return self.__criado_em
+
+    def mes_cadastro(self):
+        mes_cadastro = self.criado_em.strftime('%B').capitalize()
+        return mes_cadastro
+
+    def dia_da_semana(self):
+        dia_da_semana = self.criado_em.strftime('%A').capitalize()
+        return dia_da_semana
+
+    def data_formatada(self):
+        return self.criado_em.strftime('%d/%m/%Y %H:%M')
---
+++ br_datas_main.py
@@ -0,0 +1,8 @@
+from br_datas import DataBr
+
+data_br = DataBr()
+print(data_br)
+datas = [data_br]
+print(datas)
+print(f"Mês: {data_br.mes_cadastro()}")
+print(f"Dia da semana: {data_br.dia_da_semana()}")
